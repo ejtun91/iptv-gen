c2eedee1109b93f1b896b141b431d78b6fee4d5d
ejtun91 <ejtun91@gmail.com>
2022-03-18 19:18:33 +0100

    cors 2

--- pages/api/filedownload.js
+++ pages/api/filedownload.js
@@ -3,12 +3,20 @@ import dbConnect from "../../util/mongo";
 import stream from "stream";
 import { promisify } from "util";
 import fetch from "node-fetch";
+import NextCors from "nextjs-cors";
 
 export default async function handler(req, res) {
   const {
     method,
     query: { id },
   } = req;
+
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
 
   dbConnect();
   const https = require("https");
@@ -36,6 +44,7 @@ export default async function handler(req, res) {
       res.setHeader("Content-Type", "text/plain");
       res.setHeader("Content-Disposition", "attachment; filename= mylist.m3u");
       await pipeline(response.body, res);
+      res.send("ok");
     } catch (error) {
       res.status(500).json(error);
     }
--- pages/api/index.js
+++ pages/api/index.js
@@ -7,6 +7,11 @@ export default async function handler(req, res) {
     query: { id },
   } = req;
 
+  // This will allow OPTIONS request
+  if (method === "OPTIONS") {
+    return res.status(200).send("ok");
+  }
+
   dbConnect();
 
   if (method === "GET") {
